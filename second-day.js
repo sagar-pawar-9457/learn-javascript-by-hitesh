05f7f568ec5bd6453386a7318673dffaf3275f05
SAGAR PAWAR <163662611+sagar-pawar-9457@users.noreply.github.com>
2024-06-22 05:09:25 +0000

    second-day-done

---
+++ second-day.js
@@ -0,0 +1,106 @@
+" use strict" // treat all js code as newer version
+
+// alert("hello")// we are using node js ,not brower
+
+// console.log(3
+//  +
+//   5); // code reaadability should be high
+
+// console.log("hitesh");
+
+// let name ="hitesh"
+// let age = 18
+// let isLoggedIn = false
+
+//Number => 2 to power 53
+// bigint => 
+//string => ""
+//boolean => true /false
+//null ==> standlone value
+//undefined => 
+// symbol => unique
+
+//object
+
+// console.log(typeof age);
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+// videos no - 6  conversion Opeation
+
+// let score = 33
+
+// console.log(typeof score);
+// ********or*******
+// console.log(typeof (score));
+
+// let scoreSecond = "33"
+// console.log(typeof scoreSecond);
+
+// let valueInNumer = Number(scoreSecond)
+// console.log(typeof valueInNumer);
+
+
+// let scoreSecond = "33abc"
+// console.log(typeof scoreSecond);
+
+// let valueInNumer = Number(scoreSecond)
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
+
+// let scoreSecond = null
+// console.log(typeof scoreSecond);
+
+// let valueInNumer = Number(scoreSecond)
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
+
+// let scoreSecond = undefined
+// console.log(typeof scoreSecond);
+
+// let valueInNumer = Number(scoreSecond)
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
+
+// let scoreSecond = true
+// console.log(typeof scoreSecond);
+
+// let valueInNumer = Number(scoreSecond)
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
+
+
+
+// let scoreSecond = "hitesh"
+// console.log(typeof scoreSecond);
+
+// let valueInNumer = Number(scoreSecond)
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false =>0
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 =>true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
