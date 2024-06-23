f6004f018e2e7fb9fb1b05de1104f99b0db3e452
SAGAR PAWAR <163662611+sagar-pawar-9457@users.noreply.github.com>
2024-06-23 09:27:28 +0000

    third-day-done

---
+++ third-day.js
@@ -0,0 +1,88 @@
+// ****************** operation ********************************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.log( 3 + 3);
+// console.log(3 - 3 );
+// console.log(3 * 3 );
+// console.log(3 ** 3 );
+// console.log(3 / 3 );
+// console.log(3 % 3 );
+
+let str1 ="hello"
+let str2 =" Sagar"
+let str3 = str1 + str2 ;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 +"2");
+
+
+// console.log(3 + 4 * 5 % 3);  // avoid this readability that should be high
+
+// console.log(( 3 + 4 )*( 5 % 3)); // best option 
+
+
+
+// avoid this coversions
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+); // unexpected  syntex
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+
+
+
+// postfix and prefix example  
+// ***********postfix use after increament ************
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+
+
+// *** prefix value first increment  *********
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+// ************************* Comparison opearator ******************************
+
+
+// console.log( 2 > 1);
+// console.log( 2 < 1);
+// console.log( 2 >= 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log( null > 0);
+// console.log( null == 0);
+// console.log( null >= 0);
+
+// console.log( undefined > 0);
+// console.log(undefined < 0);
+// console.log( undefined == 0);
+// console.log(undefined >= 0);
+
+
+
+// == (check value ) vs === (check value and datatypes)
+
+console.log("2" == 2);
+console.log("2" === 2);
